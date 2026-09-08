19c0b107140b04a6b6708e8ec5fe10defe5eaaff
User <your-github-email@example.com>
2026-09-08 13:13:17 +0300

    Write data to file

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { fileLoader } = require('ejs');
 const express = require('express')
 const app = express()
 const fs = require('fs');
@@ -6,20 +7,49 @@ const path = require('path')
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+const readFile = (filename) => {
+    return new Promise((resolve, reject) => { 
+    
+        fs.readFile(filename, 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+
+            const tasks = data.split("\n")
+            resolve(tasks)
+        });
+    })
+} 
+
 app.get('/', (req, res) => {
 
-    fs.readFile('./tasks', 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        console.log(data)
-        console.log(typeof data);
-        console.log(data.split("\n"))
+    readFile('./tasks')
+        .then(tasks => {
+            console.log(tasks)
+            res.render('index', {tasks: tasks})
+        })
+})
 
-        const tasks = ["Study HTML", 'Study CSS', 'Study JS', 'Study OOP']
-        res.render('index', {tasks: tasks})
-    });
+app.use(express.urlencoded({ extended: true}));
+
+app.post('/', (req, res) => {
+
+    readFile('./tasks')
+        .then(tasks => {
+
+            tasks.push(req.body.task)
+            const data = tasks.join('\n')
+
+            fs.writeFile('./tasks', data, err => {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+
+        res.redirect('/')
+       }) 
+    })
 })
 
 app.listen(3001, () => {
